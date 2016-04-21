cb215ce1c828f3188c217cdf73c27e4f6beab19e
Tara Campbell <tac1998@gmail.com>
2016-04-21 12:05:37 -0500

    first day's work

--- controller.rb
+++ controller.rb
@@ -1,3 +1,18 @@
+require 'sinatra'
+
 get '/' do
-"Welcome to the Career Path"
+	@choice_1 = "Choose Path"
+	@choice_2 = "Graduate"
+	@choice_3 = "Dropout"
+	erb :index
+end
+
+post '/part_two' do
+	if "Dropout" == 
+		@title = "You may want to rethink!"
+		erb :part_two
+	elsif "Graduate"
+		@title = "Good Choice!"
+		erb :part_two
+	end
 end
