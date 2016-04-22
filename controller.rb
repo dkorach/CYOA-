15e80f2d9d35fbf6a4918b2684a37e61b4ebefa4
Tara Campbell <tac1998@gmail.com>
2016-04-22 11:11:36 -0500

    got it to workgit add .

--- controller.rb
+++ controller.rb
@@ -4,15 +4,26 @@ get '/' do
 	@choice_1 = "Choose Path"
 	@choice_2 = "Graduate"
 	@choice_3 = "Dropout"
+	@choice_4 = "--"
 	erb :index
 end
 
-post '/part_two' do
-	if "Dropout" == 
+post '/' do
+	if "Dropout" == params[:choice]
 		@title = "You may want to rethink!"
+		@choice_1 = "Choose Path"
+		@choice_2 = "Live with Parents"
+		@choice_3 = "Get your GED"
+		@choice_4 = "--"
 		erb :part_two
-	elsif "Graduate"
+	elsif "Graduate" == params[:choice]
 		@title = "Good Choice!"
+		@choice_1 = "Choose Path"
+		@choice_2 = "Community College"
+		@choice_3 = "4 Year University"
+		@choice_4 = "Vocational School"
 		erb :part_two
 	end
 end
+
+post '/part_two'
