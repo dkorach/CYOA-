f92c8687099d16527dc2089d2c417dde40f2515f
Alden Sulger <asulger@lsoc.org>
2016-04-26 10:21:33 -0500

    alden's edits

--- controller.rb
+++ controller.rb
@@ -24,6 +24,18 @@ post '/' do
 		@choice_4 = "Vocational School"
 		erb :part_two
 	end
+
+	 if "Live with Parents" == params[:choice2]
+	 	@ending = "Have fun living with your parents"
+		erb :ending
+	elsif "Get your GED" == params[:choice2]
+	elsif "Community College" == params[:choice2]
+	elsif "4 Year University" == params[:choice2]
+	elsif "Vocational School" == params[:choice2]
+	 	 
+			
+	 	
+	 end
 end
 
 post '/part_two'
