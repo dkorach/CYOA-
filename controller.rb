7b1ad017b0c5ff48a80fc02be6ed399d3cbc7c73
Tara Campbell <tac1998@gmail.com>
2016-04-28 14:05:23 -0500

    got it all on one erb

--- controller.rb
+++ controller.rb
@@ -1,48 +1,40 @@
 require 'sinatra'
 
 get '/' do
-	@choice_1 = "Choose Path"
-	@choice_2 = "Graduate"
-	@choice_3 = "Dropout"
-	@choice_4 = "--"
+	@choices = ["Choose Path", "Graduate", "Dropout"]
 	erb :index
 end
 
 post '/' do
 	if "Dropout" == params[:choice]
 		@title = "You may want to rethink!"
-		@choice_1 = "Choose Path"
-		@choice_2 = "Live with Parents"
-		@choice_3 = "Get your GED"
-		@choice_4 = "--"
-		erb :part_two
+		@choices = ["Choose Path", "Live with Parents", "Get your GED"]
+		erb :index
 	elsif "Graduate" == params[:choice]
 		@title = "Good Choice!"
-		@choice_1 = "Choose Path"
-		@choice_2 = "Community College"
-		@choice_3 = "4 Year University"
-		@choice_4 = "Vocational School"
-		erb :part_two
-	end
-end
-
-post '/part_two' do
-	puts params[:choice]
-	 if "Live" == params[:choice]
+		@choices = ["Choose Path", "Community College", "4 Year University", "Vocational School"]
+		erb :index
+	 elsif "Live" == params[:choice]
 	 	@title = "Failure"
 	 	@ending = "Have fun living with your parents :("
-	 	erb :ending
-	elsif "Get your GED" == params[:choice]
+	 	erb :index
+	elsif "Get" == params[:choice]
 		@title = "Not bad considering the circumstances"
 		@ending = "You will work at a low paying job"
 		@imagesource = "https://media.glassdoor.com/o/432/mcdonald-s-office.jpg"
-		erb :ending
-	elsif "Community College" == params[:choice]
-		@choice1 = ""
-	elsif "4 Year University" == params[:choice] 
-		@choice1 = "Graduate School"
-	elsif "Vocational School" == params[:choice]
-	 	 	
+		erb :index
+	elsif "Community" == params[:choice]
+		@title = "Way to finish school!"
+		@ending = "You will work at a low paying job"
+		erb :index
+	elsif "4" == params[:choice] 
+		@title = "Nice job getting in!"
+		@ending = "You'll make bank"
+		erb :index
+	elsif "Vocational" == params[:choice]
+	 	 @title = "You know what you want to be!"
+	 	 @ending = "You'll work in a field specific job"
+	 	 erb :index
 	 end
 end
 
