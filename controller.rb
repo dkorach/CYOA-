9b01a7fab17bf4fd82b6bfc12c48850f0ff28e21
Daniel Korac <dkorach@lsoc.org>
2016-04-28 14:21:24 -0500

    final additions

--- controller.rb
+++ controller.rb
@@ -17,6 +17,7 @@ post '/' do
 	 elsif "Live" == params[:choice]
 	 	@title = "Failure"
 	 	@ending = "Have fun living with your parents :("
+	 	@imagesource = "http://cdn.financialsamurai.com/wp-content/uploads/2010/08/livingathome-300x203.jpg"
 	 	erb :index
 	elsif "Get" == params[:choice]
 		@title = "Not bad considering the circumstances"
@@ -26,14 +27,17 @@ post '/' do
 	elsif "Community" == params[:choice]
 		@title = "Way to finish school!"
 		@ending = "You will work at a low paying job"
+		@imagesource = "http://i.imgur.com/kLFfRsD.jpg"
 		erb :index
 	elsif "4" == params[:choice] 
 		@title = "Nice job getting in!"
 		@ending = "You'll make bank"
+		@imagesource = "http://www.apeshitmedia.com/lj/2010-01-01-01.jpg"
 		erb :index
 	elsif "Vocational" == params[:choice]
 	 	 @title = "You know what you want to be!"
 	 	 @ending = "You'll work in a field specific job"
+	 	 @imagesource = "http://www.featurepics.com/FI/Thumb300/20071229/Funny-Plumber-558715.jpg"
 	 	 erb :index
 	 end
 end
